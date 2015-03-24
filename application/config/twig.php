a400d12f98d46471f9335e367d0deaa7660f1568
Lorenzo De Leon Alipio <Lorenzo-D-Alipio@users.noreply.github.com>
2015-03-24 12:57:05 -0700

    Create twig.php
    added config file for  twig. application/config/twig.php

---
+++ application/config/twig.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+/*
+**
+** The MIT License (MIT)
+**
+** Copyright (c) 2015 Lorenzo De Leon Alipio
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions:
+** 
+** The above copyright notice and this permission notice shall be included in all
+** copies or substantial portions of the Software.
+
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
+**
+**
+*/
+
+
+
+$config = array(
+				
+				'cache_status' => true,
+				'cache_lifetime'=> 3600,
+				'theme_path_t' => APPPATH.'views/themes/twig/',
+				'cache_directory_t' => APPPATH.'views/theme_cache/twig/',
+				'auto_reload_t' => true
+				
+				);
+
+
+/*
+* end of application/config/twig.php
+*/
